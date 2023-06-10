1e495fb714349007d36fe9d940382e0f771039c4
ah-rifat <rifat266@gmail.com>
2023-06-11 01:15:29 +0600

    classes status approve/deny route change -done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Server Running...");
 });
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.c8viahs.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -74,6 +74,20 @@ async function run() {
         },
       };
       const result = await users.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // set classes status - approve/deny
+    app.patch("/classesStatus/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.query.status;
+      const query = { _id: new ObjectId(id) };
+      const updateStatus = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = classes.updateOne(query, updateStatus);
       res.send(result);
     });
 
